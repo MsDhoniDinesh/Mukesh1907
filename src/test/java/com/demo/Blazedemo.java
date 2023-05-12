f340bed3f1068ba85b20f86c449001da54361883
MsDhoniDinesh <DineshKumar079677>
2023-05-12 10:24:57 +0530

    Added

---
+++ src/test/java/com/demo/Blazedemo.java
@@ -0,0 +1,23 @@
+package com.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import Parent.Baseclass;
+
+public class Blazedemo extends Baseclass {
+
+	@Test
+	public void login() {
+		Loginpage lp = new Loginpage();
+		chrome();
+		implicitwait();
+		url("https://blazedemo.com/index.php");
+		clickbutton(lp.getLink());
+	//	WebElement link = driver.findElement(By.xpath("//a[text()='destination of the week! The Beach!']"));
+		//clickbutton(link);
+
+	}
+
+}
